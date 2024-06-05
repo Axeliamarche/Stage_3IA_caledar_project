d4a93b5b1f98844b64444f2ff2549b731738e358
AndriiSmal <andrii.smal@allievi.bearzi.it>
2024-06-05 11:43:10 +0200

    (2)Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// main.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -32,19 +34,19 @@ class ReservationPageState extends State<ReservationPage> {
   String? _loggedInUser;
   int _selectedIndex = 0;
   late ReservationModel _reservationModel;
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
     _reservationModel = Provider.of<ReservationModel>(context, listen: false);
-     _timer = Timer.periodic(Duration(minutes: 1), (timer) {
+    _timer = Timer.periodic(Duration(minutes: 1), (timer) {
       _reservationModel.removeExpiredReservations();
     });
   }
-  
+
   @override
   void dispose() {
-    // Cancella il timer quando il widget viene distrutto
     _timer.cancel();
     super.dispose();
   }
@@ -75,6 +77,7 @@ class ReservationPageState extends State<ReservationPage> {
         child: _selectedIndex == 0 ? _buildCalendarPage() : _buildWarehousePage(),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 20,
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         items: [
@@ -95,106 +98,116 @@ class ReservationPageState extends State<ReservationPage> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TableCalendar(
-          focusedDay: DateTime.now(),
-          firstDay: DateTime(2000),
-          lastDay: DateTime(2100),
-          calendarFormat: _calendarFormat,
-          availableCalendarFormats: const {
-            CalendarFormat.month: 'Week',
-            CalendarFormat.week: 'Month',
-          },
-          selectedDayPredicate: (day) {
-            return day.isAfter(DateTime.now().subtract(Duration(days: 1))) && _reservationModel.hasReservation(day);
-          },
-          calendarStyle: CalendarStyle(
-            selectedDecoration: BoxDecoration(
-              color: Colors.blue.withOpacity(0.5),
-            ),
-          ),
-          onDaySelected: (selectedDay, focusedDay) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => RobotPage(selectedDay: selectedDay),
+        Consumer<ReservationModel>(
+          builder: (context, reservationModel, child) {
+            return TableCalendar(
+              focusedDay: DateTime.now(),
+              firstDay: DateTime(2000),
+              lastDay: DateTime(2100),
+              calendarFormat: _calendarFormat,
+              availableCalendarFormats: const {
+                CalendarFormat.month: 'Week',
+                CalendarFormat.week: 'Month',
+              },
+              selectedDayPredicate: (day) {
+                return day.isAfter(DateTime.now().subtract(Duration(days: 1))) && reservationModel.hasReservation(day);
+              },
+              calendarStyle: CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.5),
+                ),
               ),
+              onDaySelected: (selectedDay, focusedDay) {
+                if (selectedDay.isAfter(DateTime.now().subtract(Duration(days: 1)))) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RobotPage(selectedDay: selectedDay),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Non puoi selezionare una data passata.')),
+                  );
+                }
+              },
             );
           },
         ),
         SizedBox(height: 20),
-            Consumer<ReservationModel>(
-              builder: (context, reservationModel, child) {
-                final reservations = reservationModel.getReservations();
+        Consumer<ReservationModel>(
+          builder: (context, reservationModel, child) {
+            final reservations = reservationModel.getReservations();
+            return Column(
+              children: reservations.map((reservation) {
+                final isUserEvent = reservation.name == _loggedInUser;
                 return Column(
-                  children: reservations.map((reservation) {
-                    final isUserEvent = reservation.name == _loggedInUser;
-                    return Column(
-                      children: [
-                        Text(
-                          'Prenotazione di ${reservation.name}: ${reservation.startTime} - ${reservation.endTime}, ${reservation.selectedRobots.length} robot',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        if (isUserEvent)
-                          ElevatedButton(
-                            onPressed: () {
-                              reservationModel.removeReservation(reservation.date);
-                            },
-                            child: Text('Elimina'),
-                          ),
-                      ],
-                    );
-                  }).toList(),
+                  children: [
+                    Text(
+                      'Prenotazione di ${reservation.name}: ${reservation.startTime} - ${reservation.endTime}, ${reservation.selectedRobots.length} robot',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    if (isUserEvent)
+                      ElevatedButton(
+                        onPressed: () {
+                          reservationModel.removeReservation(reservation.date);
+                        },
+                        child: Text('Elimina'),
+                      ),
+                  ],
                 );
-              },
-            ),
+              }).toList(),
+            );
+          },
+        ),
       ],
     );
   }
 
   Widget _buildWarehousePage() {
-  final currentDate = _calendarFormat == CalendarFormat.week ? DateTime.now() : DateTime.now().subtract(Duration(days: DateTime.now().day - 1));
-  final totalAvailableRobots = _calendarFormat == CalendarFormat.month
-      ? _reservationModel.getTotalAvailableRobotsInMonth(currentDate, _reservationModel.getReservations())
-      : _reservationModel.getTotalAvailableRobotsInWeek(currentDate, _reservationModel.getReservations());
+    final currentDate = _calendarFormat == CalendarFormat.week ? DateTime.now() : DateTime.now().subtract(Duration(days: DateTime.now().day - 1));
+    final totalAvailableRobots = _calendarFormat == CalendarFormat.month
+        ? _reservationModel.getTotalAvailableRobotsInMonth(currentDate, _reservationModel.getReservations())
+        : _reservationModel.getTotalAvailableRobotsInWeek(currentDate, _reservationModel.getReservations());
 
-  return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Warehouse for ${_calendarFormat == CalendarFormat.month ? 'Month' : 'Week'}',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 20),
-        Text(
-          'Total Available Robots: $totalAvailableRobots',
-          style: TextStyle(fontSize: 18),
-        ),
-        SizedBox(height: 20),
-        Expanded(
-          child: Consumer<ReservationModel>(
-            builder: (context, reservationModel, child) {
-              final reservations = reservationModel.getReservations();
-              return ListView.builder(
-                itemCount: reservations.length,
-                itemBuilder: (context, index) {
-                  final reservation = reservations[index];
-                  return ListTile(
-                    title: Text('Prenotazione di ${reservation.name}'),
-                    subtitle: Text('${reservation.startTime} - ${reservation.endTime}, ${reservation.selectedRobots.length} robot'),
-                  );
-                },
-              );
-            },
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Warehouse for ${_calendarFormat == CalendarFormat.month ? 'Month' : 'Week'}',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-        ),
-      ],
-    ),
-  );
-}
+          SizedBox(height: 20),
+          Text(
+            'Total Available Robots: $totalAvailableRobots',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: Consumer<ReservationModel>(
+              builder: (context, reservationModel, child) {
+                final reservations = reservationModel.getReservations();
+                return ListView.builder(
+                  itemCount: reservations.length,
+                  itemBuilder: (context, index) {
+                    final reservation = reservations[index];
+                    return ListTile(
+                      title: Text('Prenotazione di ${reservation.name}'),
+                      subtitle: Text('${reservation.startTime} - ${reservation.endTime}, ${reservation.selectedRobots.length} robot'),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   void _onItemTapped(int index) {
     setState(() {
